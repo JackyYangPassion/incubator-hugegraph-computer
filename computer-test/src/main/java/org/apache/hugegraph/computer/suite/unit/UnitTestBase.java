b9482ce0abc67a9a5f4b142b1c7f8f6fd39befe4
YangJiaqi <yang.jiaqi.hadoop@gmail.com>
2023-04-19 15:47:40 +0800

    for algo test local

--- computer-test/src/main/java/org/apache/hugegraph/computer/suite/unit/UnitTestBase.java
+++ computer-test/src/main/java/org/apache/hugegraph/computer/suite/unit/UnitTestBase.java
@@ -81,10 +81,10 @@ public class UnitTestBase {
 
         Whitebox.setInternalState(ComputerOptions.BSP_ETCD_ENDPOINTS,
                 "defaultValue",
-                "http://localhost:2579");
+                "http://localhost:2379");
         Whitebox.setInternalState(ComputerOptions.HUGEGRAPH_URL,
                 "defaultValue",
-                "http://127.0.0.1:8080");
+                "http://127.0.0.1:8082");
         Whitebox.setInternalState(ComputerOptions.HUGEGRAPH_GRAPH_NAME,
                 "defaultValue",
                 "hugegraph");
